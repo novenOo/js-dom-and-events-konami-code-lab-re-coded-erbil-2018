a1fe42eac2c0048049150bf67955863ef59c360d
noven temer <noven5823@gmail.com>
2018-09-24 10:18:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
+const index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
+ document.body.addEventListener('keydown',e =>{
+   const key = parseInt(e.which || e.detail);
+   
+   if(code[index] === key){
+   index++;
 
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
